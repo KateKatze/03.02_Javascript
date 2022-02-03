a353800c59af486ef78eb59d7e8e40c9f76cbf9a
KateKatze <dr.katekogler@gmail.com>
2022-02-03 15:05:05 +0100

    changes sizes of the columns compare to the devices

--- Challenge/script.js
+++ Challenge/script.js
@@ -13,30 +13,18 @@ for (let value of arr) {
     `;
 }
 
-// function enlargeImg(arr, i) {
-//     img.style.width = "60%";
-//     img.style.height = "auto";
-//     img.style.transition = "width 0.5s ease";
-// }
-
-// function resetImg() {
-//     img.style.width = "40%";
-//     img.style.height = "auto";
-//     img.style.transition = "width 0.5s ease";
-// }
-
 function moreDetails(index) {
     return `
-<div class="col-12">
-<div class="card cardDetails m-2">
-<img src="${arr[index].img}" class="card-img-top" alt="${arr[index].name}">
-<div class="card-body">
+  <div class="col-lg-7 col-md-8 col-sm-9">
+  <div class="card cardDetails m-2">
+  <img src="${arr[index].img}" class="card-img-top" alt="${arr[index].name}">
+  <div class="card-body">
   <h5 class="card-title">${arr[index].name}, ${arr[index].production_year}</h5>
   <p class="card-text">${arr[index].car_price} <br> ${arr[index].color} <br> ${arr[index].transmission}</p>
-</div>
-</div>
-</div>
-`;
+  </div>
+  </div>
+  </div>
+  `;
 }
 
 var cardDetails = document.getElementsByClassName("cardDetails");
