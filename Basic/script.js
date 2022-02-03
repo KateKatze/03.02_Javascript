24c42df98ec1fa13afbe8bd9a68d928ea6f6f7e2
KateKatze <dr.katekogler@gmail.com>
2022-02-03 11:37:56 +0100

    Deleted unnecessary files

--- Basic/script.js
+++
@@ -1,4 +0,0 @@
-var newFood = JSON.parse(food);
-console.table(newFood);
-
-document.getElementById("results").innerHTML = `My favourite sandwich is a ${newFood[0].sandwich} which has approximately ${newFood[0].calories} calories, along with it I enjoy eating ${newFood[1]friesSize} which have about ${newFood[1].calories} calories`;
--- README.md
+++
@@ -1,1 +0,0 @@
-# 03.02_Javascript
